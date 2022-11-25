c7e9ec6798a279610b792c549d8775f280acaed1
rawnaqul <slow.coach.rawnaq@gmail.com>
2022-11-25 23:54:08 +0600

    product details added

--- src/Pages/Home/Home/Banner.js
+++ src/Pages/Home/Home/Banner.js
@@ -11,7 +11,7 @@ const Banner = () => {
                     <img className='rounded-xl opacity-75' src="https://i.postimg.cc/cCZtgT0q/pexels-pixabay-159618.jpg" alt="" />
                 </div>
                 <div className='my-auto mr-auto'>
-                    <h1 className='font-bold text-4xl lg:text-7xl lg:leading-[70px] mt-5 text-primary'>Never Stop Reading</h1>
+                    <h1 className='font-bold text-4xl lg:text-7xl lg:leading-[70px] mt-5 text-primary font-serif'>Never Stop Reading</h1>
                     <h2 className='lg:my-8 my-4 text-white lg:text-xl '>A home for those who love to read books and sell their old books.</h2>
                     <p className='text-white w-96 '>Buying a book is similar to shopping for a bibliomaniac and used books may cost you only half the rate of new books. By buying them, you may cut costs and buy more books with your saved money. Unlike the cozy, shiny book shops, you have the option to bargain in second-hand shops. You can showcase all your negotiating talent and buy as many books as possible.
                     </p>
--- src/Pages/ProductList/ProductList.js
+++ src/Pages/ProductList/ProductList.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Link, useLoaderData } from 'react-router-dom';
-import PrimaryButton from '../../Components/Buttons/PrimaryButton';
+import { useLoaderData } from 'react-router-dom';
 
 const ProductList = () => {
     const products = useLoaderData();
@@ -11,7 +10,7 @@ const ProductList = () => {
             <h1 className='text-3xl font-semibold font-serif text-slate-600 bg-green-200 p-5 rounded-xl text-center'>Available {categoryName} are shown below!</h1>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-24 container mx-auto'>
                 {
-                    products.map(product => <label htmlFor="my-modal-3" key={product._id} className="card w-96 bg-base-100 shadow-xl cursor-pointer">
+                    products.map(product => <label htmlFor={`my-modal-${product._id}`} key={product._id} className="card w-96 bg-base-100 shadow-xl cursor-pointer">
                         <figure><img src={product.image} alt="pictures of books" /></figure>
                         <div className="card-body">
                             <h2 className="card-title">
@@ -24,14 +23,14 @@ const ProductList = () => {
                                 <div className="badge badge-outline bg-orange-100">Price: ${product.resalePrice}</div>
                             </div>
                             {/* Put this part before </body> tag */}
-                            <input type="checkbox" id="my-modal-3" className="modal-toggle" />
+                            <input type="checkbox" id={`my-modal-${product._id}`} className="modal-toggle" />
                             <div className="modal">
                                 <div className="modal-box relative">
-                                    <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                                    <label htmlFor={`my-modal-${product._id}`} className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
                                     <figure className=''><img src={product.image} alt="pictures of books" className='rounded' /></figure>
                                     <br />
                                     <h3 className="text-lg font-bold">{product.name}</h3>
-                                    <p className="py-4"><span className='text-thin text-slate-400'>Details:</span> {product.description}..</p>
+                                    <p className=""><span className='text-thin text-slate-400'>Details:</span> {product.description}..</p>
                                     <p><span className='text-thin text-slate-400'>Original Price:</span> ${product.originalPrice}</p>
                                     <p><span className='text-thin text-slate-400'>Years Of Use:</span> {product.yearsOfUse}</p>
                                     <p><span className='text-thin text-slate-400'>Posted On:</span> {product.postDate}</p>
@@ -40,11 +39,11 @@ const ProductList = () => {
                                     <p><span className='text-thin text-slate-400'>Seller Phone Number: </span>+88{product.phone}</p>
                                     <div className="badge badge-outline bg-green-100">Condition: {product.condition}</div>
                                     <br />
-                                    <div className="badge badge-outline bg-orange-100">Price: ${product.resalePrice}</div>
+                                    <div className="badge badge-outline bg-orange-100 mt-1">Price: ${product.resalePrice}</div>
                                     <br />
                                     <br />
                                     <div className='w-full flex justify-end'>
-                                        <button className='btn border-none bg-gradient-to-r from-blue-400 to-secondary text-white'>Book Now!</button>
+                                        <button className='btn border-none bg-gradient-to-r from-blue-400 to-lime-100 text-white'>Book Now!</button>
                                     </div>
                                 </div>
                             </div>
